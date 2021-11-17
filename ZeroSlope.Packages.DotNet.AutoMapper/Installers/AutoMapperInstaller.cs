378182b87ecdafb4dac3e018a9ae6e36fda07fcf
cfryerdev <cfryerdev@gmail.com>
2021-11-17 15:07:25 -0700

    Initial commit

---
+++ ZeroSlope.Packages.DotNet.AutoMapper/Installers/AutoMapperInstaller.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using AutoMapper.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZeroSlope.Packages.DotNet.AutoMapper.Installers
+{
+    public class AutoMapperInstaller
+    {
+		private readonly MapperConfigurationExpression _expression;
+
+		public AutoMapperInstaller(MapperConfigurationExpression cfg)
+        {
+			_expression = cfg;
+		}
+
+		public void Install(IServiceCollection serviceCollection)
+		{
+			var configuration = new MapperConfiguration(_expression);
+			var mapper = new Mapper(configuration);
+			serviceCollection.AddSingleton<IMapper>(mapper);
+		}
+	}
+}
